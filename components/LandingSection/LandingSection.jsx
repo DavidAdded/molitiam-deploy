0d1a2f1855662c6414adc65d4c85b4a51a1824ed
Oliver-lundkvist <oliver@added.digital>
2023-12-21 15:24:54 +0100

    GSAP animation header

--- components/LandingSection/LandingSection.jsx
+++ components/LandingSection/LandingSection.jsx
@@ -1,18 +1,52 @@
+"use client"
+
 import "./LandingSection.css";
+import gsap from "gsap";
+import ScrollTrigger from "@utils/ScrollTrigger";
+import SplitText from "@utils/SplitText";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 
 const LandingSection = () => {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const triggerElement = sectionRef.current.querySelector('.landing-content-wrapper');
+    const targetElements = sectionRef.current.querySelectorAll('.animation-header');
+  
+    targetElements.forEach(header => {
+      // Split the text of each header
+      const split = new SplitText(header, { type: "lines" });
+  
+      // Animate all lines at once with a stagger
+      gsap.from(split.lines, {
+        scrollTrigger: {
+          trigger: triggerElement,
+          start: "top center",
+          end: "bottom top",
+          toggleActions: "restart none none reset"
+        },
+        opacity: 0,
+        y: 20,
+        duration: 0.5,
+        stagger: 0.1  // Stagger applied to the collection of lines
+      });
+    });
+  }, []);
   return (
     <div className="landing-section-wrapper">
       <div className="padding-global">
         <div className="container-large">
           <div className="padding-section-small">
-            <div className="landing-content-wrapper">
+            <div ref={sectionRef} className="landing-content-wrapper">
               <div className="picture-wrapper">
                 <img src="/camoimage.png" alt="Landing picture" />
                 <div className="landing-grey-image"></div>
               </div>
               <div className="text-wrapper">
-                <h1>CYBERSÄKERHET FÖR SAMHÄLLSVIKTIG VERKSAMHET</h1>
+                <h1 className="animation-header">CYBERSÄKERHET FÖR SAMHÄLLSVIKTIG VERKSAMHET</h1>
                 <p>Försvarsteknologi. Säkert. Enkelt.</p>
               </div>
             </div>
@@ -22,5 +56,6 @@ const LandingSection = () => {
     </div>
   );
 };
+
 
 export default LandingSection;
