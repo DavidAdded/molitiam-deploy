0841914cc7c67c5ac20cb361ae18948e0cf3b450
philipadded <philip@added.digital>
2023-12-28 15:22:15 +0100

    News section

--- components/Nyheter/Nyheter.jsx
+++ components/Nyheter/Nyheter.jsx
@@ -1,44 +1,88 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import './Nyheter.css';
+import "./Nyheter.css";
 
 const Nyheter = () => {
-  const [gridItems, setGridItems] = useState([]);
-  const gridWidth = 20; // Number of squares per row
-  const gridHeight = 8; // Number of squares per column
-  const squareSize = 90; // Size of squares in pixels
-  const opacityVariance = 0.1; // Maximum variance in opacity between adjacent squares
-
-  useEffect(() => {
-    const items = [];
-    let lastOpacity = Math.random(); // Initial opacity for the first square
-
-    for (let i = 0; i < gridWidth * gridHeight; i++) {
-      // Determine new opacity within 10% of the last square
-      let minOpacity = Math.max(lastOpacity - opacityVariance, 0);
-      let maxOpacity = Math.min(lastOpacity + opacityVariance, 1);
-      let newOpacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
-
-      // Add new square with new opacity
-      items.push(
-        <div key={i} className="grid-item" style={{ opacity: newOpacity }}></div>
-      );
-
-      // Update lastOpacity for the next iteration
-      lastOpacity = newOpacity;
-    }
-
-    setGridItems(items);
-  }, [gridWidth, gridHeight, opacityVariance]);
-
   return (
-    <div className='nyheter-section-wrapper'>
-      <div className="grid-background" style={{ gridTemplateColumns: `repeat(${gridWidth}, ${squareSize}px)` }}>
-        {gridItems}
+    <div className="nyheter-section-wrapper">
+      <div className="padding-global">
+        <div className="container-large">
+          <div className="padding-section-large">
+            <div className="nyheter-content-wrapper">
+              <div className="nyheter-text-wrapper">
+                <img src="/prefix-icon.svg" alt="Left" />
+                <h6> Nyheter </h6>
+              </div>
+              <div className="nyheter-content-boxes">
+                <div className="nyheter-content-card">
+                  <div className="nyheter-content-card-top-left"></div>
+                  <div className="nyheter-content-card-bottom">
+                    <div className="nyheter-content-card-text-wrapper">
+                      <div className="nyheter-date">2024•01•01</div>
+                      <h3>Lorem ipsum dolor sit amet</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna.
+                      </p>
+                    </div>
+                    <div className="nyheter-las-mer">
+                      <div className="nyheter-las-mer-content">
+                        <p>LÄS MER</p>
+                        <img src="/right-arrow.svg" />
+                      </div>
+                    </div>
+                    <div className="bottom-corner-cover-up"></div>
+                  </div>
+                </div>
+                <div className="nyheter-content-card">
+                  <div className="nyheter-content-card-top-middle"></div>
+                  <div className="nyheter-content-card-bottom">
+                    <div className="nyheter-content-card-text-wrapper">
+                      <div className="nyheter-date">2024•01•01</div>
+                      <h3>Lorem ipsum dolor sit amet</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna.
+                      </p>
+                    </div>
+                    <div className="nyheter-las-mer">
+                      <div className="nyheter-las-mer-content">
+                        LÄS MER
+                        <img src="/right-arrow.svg" />
+                      </div>
+                    </div>
+                    <div className="bottom-corner-cover-up"></div>
+                  </div>
+                </div>
+                <div className="nyheter-content-card">
+                  <div className="nyheter-content-card-top-right"></div>
+                  <div className="nyheter-content-card-bottom">
+                    <div className="nyheter-content-card-text-wrapper">
+                      <div className="nyheter-date">2024•01•01</div>
+                      <h3>Lorem ipsum dolor sit amet</h3>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna.
+                      </p>
+                    </div>
+                    <div className="nyheter-las-mer">
+                      <div className="nyheter-las-mer-content">
+                        LÄS MER
+                        <img src="/right-arrow.svg" />
+                      </div>
+                    </div>
+                    <div className="bottom-corner-cover-up"></div>
+                  </div>
+                </div>
+              </div>
+              <button> LÄS ALLA NYHETER</button>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* Other content */}
     </div>
   );
-}
+};
 
 export default Nyheter;
